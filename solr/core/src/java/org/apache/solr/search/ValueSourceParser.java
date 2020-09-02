49c99206cb422fd3e706a4290f4a93c69c0d19a2
Matthew Kavanagh <mattk@brandwatch.com>
2020-09-02 13:22:08 +0100

    bitmap agg to get freq of int32 vals

--- solr/core/src/java/org/apache/solr/search/ValueSourceParser.java
+++ solr/core/src/java/org/apache/solr/search/ValueSourceParser.java
@@ -57,14 +57,15 @@ import org.apache.solr.schema.TextField;
 import org.apache.solr.search.facet.AggValueSource;
 import org.apache.solr.search.facet.AvgAgg;
 import org.apache.solr.search.facet.BitmapCollectorAgg;
+import org.apache.solr.search.facet.BitmapFrequencyAgg;
 import org.apache.solr.search.facet.CountAgg;
 import org.apache.solr.search.facet.HLLAgg;
 import org.apache.solr.search.facet.MinMaxAgg;
 import org.apache.solr.search.facet.PercentileAgg;
+import org.apache.solr.search.facet.RelatednessAgg;
 import org.apache.solr.search.facet.StddevAgg;
 import org.apache.solr.search.facet.SumAgg;
 import org.apache.solr.search.facet.SumsqAgg;
-import org.apache.solr.search.facet.RelatednessAgg;
 import org.apache.solr.search.facet.TermFrequencyOfFrequenciesAgg;
 import org.apache.solr.search.facet.TopDocsAgg;
 import org.apache.solr.search.facet.UniqueAgg;
@@ -1061,6 +1062,8 @@ public abstract class ValueSourceParser implements NamedListInitializedPlugin {
     addParser("agg_termfreqfreq", new TermFrequencyOfFrequenciesAgg.Parser());
 
     addParser("agg_bitmapcollector", new BitmapCollectorAgg.Parser());
+
+    addParser("agg_bitmapfreq", new BitmapFrequencyAgg.Parser());
 
     addParser("childfield", new ChildFieldValueSourceParser());
   }
